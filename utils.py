60aa3c94395852d4bd48d80ba2a40588439a01a0
Gaia <89006415+GaiaSab@users.noreply.github.com>
2021-08-26 12:57:53 +0200

    commentato utils.py

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@
 import copy
 import torch
 from torchvision import datasets, transforms
-from sampling import mnist_iid, mnist_noniid, mnist_noniid_unequal
+#from sampling import mnist_iid, mnist_noniid, mnist_noniid_unequal
 from sampling import cifar_iid, cifar_noniid
 
 
@@ -15,60 +15,65 @@ def get_dataset(args):
     each of those users.
     """
 
-    if args.dataset == 'cifar':
-        data_dir = '../data/cifar/'
-        apply_transform = transforms.Compose(
-            [transforms.ToTensor(),
-             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    #if args.dataset == 'cifar':
+    data_dir = '../data/cifar/'
+    transform = transforms.Compose([
+                                transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-        train_dataset = datasets.CIFAR10(data_dir, train=True, download=True,
-                                       transform=apply_transform)
+    train_dataset = datasets.CIFAR10(root=data_dir,
+                                     train=True,
+                                     transform=transform,
+                                     download=True)
 
-        test_dataset = datasets.CIFAR10(data_dir, train=False, download=True,
-                                      transform=apply_transform)
+    test_dataset = datasets.CIFAR10(root=data_dir,
+                                    train=False,
+                                    transform=transform,
+                                    download=True)
+
+    # sample training data amongst users
+    if args.iid:
+        # Sample IID user data from Mnist
+        user_groups = cifar_iid(train_dataset, args.num_users)
+    else:
+        # Sample Non-IID user data from Mnist
+        if args.unequal:
+            # Chose uneuqal splits for every user
+            # - TODO??
+            raise NotImplementedError()
+
+        else:
+            # Chose equal splits for every user
+            user_groups = cifar_noniid(train_dataset, args.num_users)
+
+   # elif args.dataset == 'mnist' or 'fmnist':
+       # if args.dataset == 'mnist':
+            #data_dir = '../data/mnist/'
+       # else:
+            #data_dir = '../data/fmnist/'
+
+       # apply_transform = transforms.Compose([
+          #  transforms.ToTensor(),
+           # transforms.Normalize((0.1307,), (0.3081,))])
+
+        #train_dataset = datasets.MNIST(data_dir, train=True, download=True,
+         #                              transform=apply_transform)
+
+        #test_dataset = datasets.MNIST(data_dir, train=False, download=True,
+         #                             transform=apply_transform)
 
         # sample training data amongst users
-        if args.iid:
+        #if args.iid:
             # Sample IID user data from Mnist
-            user_groups = cifar_iid(train_dataset, args.num_users)
-        else:
+          #  user_groups = mnist_iid(train_dataset, args.num_users)
+        #else:
             # Sample Non-IID user data from Mnist
-            if args.unequal:
+         #   if args.unequal:
                 # Chose uneuqal splits for every user
-                # - TODO??
-                raise NotImplementedError()
-            else:
-                # Chose equal splits for every user
-                user_groups = cifar_noniid(train_dataset, args.num_users)
-
-    elif args.dataset == 'mnist' or 'fmnist':
-        if args.dataset == 'mnist':
-            data_dir = '../data/mnist/'
-        else:
-            data_dir = '../data/fmnist/'
-
-        apply_transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))])
-
-        train_dataset = datasets.MNIST(data_dir, train=True, download=True,
-                                       transform=apply_transform)
-
-        test_dataset = datasets.MNIST(data_dir, train=False, download=True,
-                                      transform=apply_transform)
-
-        # sample training data amongst users
-        if args.iid:
-            # Sample IID user data from Mnist
-            user_groups = mnist_iid(train_dataset, args.num_users)
-        else:
-            # Sample Non-IID user data from Mnist
-            if args.unequal:
-                # Chose uneuqal splits for every user
-                user_groups = mnist_noniid_unequal(train_dataset, args.num_users)
-            else:
+          #      user_groups = mnist_noniid_unequal(train_dataset, args.num_users)
+           # else:
                 # Chose euqal splits for every user
-                user_groups = mnist_noniid(train_dataset, args.num_users)
+            #    user_groups = mnist_noniid(train_dataset, args.num_users)
 
     return train_dataset, test_dataset, user_groups
 
