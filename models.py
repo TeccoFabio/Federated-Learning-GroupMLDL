93eb1f32d2198b4c329d51fe7739550db3648e61
Gaia <89006415+GaiaSab@users.noreply.github.com>
2021-09-03 19:33:14 +0200

    aggiornati modelli LeNet5 e CNNCifar

--- models.py
+++ models.py
@@ -84,13 +84,13 @@ class LeNet5(nn.Module):
         self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
-        x = self.pool1(F.relu(self.conv1(x))) # we can also try leaky relu
-        x = self.pool2(F.relu(self.conv2(x)))
+        x = self.pool1(torch.tanh(self.conv1(x))) # we can also try leaky relu
+        x = self.pool2(torch.tanh(self.conv2(x)))
         # Flatten all dimensions
         x = torch.flatten(x, 1)
         #x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        x = torch.tanh(self.fc1(x))
+        x = torch.tanh(self.fc2(x))
         x = self.fc3(x)
         return F.log_softmax(x, dim=1)
 
@@ -99,11 +99,13 @@ class CNNCifar(nn.Module):
         super(CNNCifar, self).__init__()
         # 1st convolutional layer, output size 28*28*6
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5)
+        self.batch1 = nn.BatchNorm2d(num_features=6)
         self.drop1 = nn.Dropout2d(p=0.5)
         # 1st pooling layer, output size 14*14*6 (we can also try maxpooling or minpooling while LeNet uses Avgpooling)
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
         # 2nd conv layer, out size 10*10*16
         self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5)
+        self.batch2 = nn.BatchNorm2d(num_features=16)
         self.drop2 = nn.Dropout2d(p=0.5)
         # 2nd pooling layer, out size 16*5*5
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
@@ -115,16 +117,18 @@ class CNNCifar(nn.Module):
     def forward(self, x):
         # Note that relu6 is performing better than relu activation function! (see paper)
         x = self.conv1(x)
-        x = self.drop1(x)
-        x = self.pool1(F.relu6(x)) # we can also try leaky relu
+        x = self.batch1(x)
+        #x = self.drop1(x)
+        x = self.pool1(F.leaky_relu(x)) # we can also try leaky relu
         x = self.conv2(x)
+        x = self.batch2(x)
         #x = self.drop2(x)
-        x = self.pool2(F.relu6(x))
+        x = self.pool2(F.leaky_relu(x))
         # Flatten all dimensions
         x = torch.flatten(x, 1)
         #x = x.view(-1, 16 * 5 * 5)
-        x = F.relu6(self.fc1(x))
-        x = F.relu6(self.fc2(x))
+        x = F.leaky_relu(self.fc1(x))
+        x = F.leaky_relu(self.fc2(x))
         x = self.fc3(x)
         return F.log_softmax(x, dim=1)
 
